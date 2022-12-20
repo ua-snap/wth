394c8660f5c2de667c522b4a34b438a5703d1782
Bruce Crevensten <bruce.crevensten@gmail.com>
2022-12-20 12:13:56 -0800

    dynamic queries working

--- server/application.py
+++ server/application.py
@@ -1,21 +1,26 @@
 from flask import Flask, Response, request
 import json
 import psycopg2
+from pyproj import Transformer
 
 app = Flask(__name__)
 
 conn = psycopg2.connect("host=localhost user=postgres password=mysecretpassword")
 
+transformer = Transformer.from_crs(4326, 3338)
 
 @app.route("/")
 def fetch_polys():
     lat = request.args.get("lat")
     lng = request.args.get("lng")
     radiusKm = request.args.get("r")
-    print(lat, lng, radiusKm)
+    coords = transformer.transform(lat, lng)
+    params = (coords[0], coords[1], float(radiusKm))
+    print(params)
+
     cur = conn.cursor()
-    sql = "SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(t.*)::json) ) FROM (select id, name, alt_name, ST_Transform( geom, 4326 ) as geom from merged_snap_polys m where ST_DWithin(m.geom, 'SRID=3338;POINT(303784.486450 1666276.643171)', 40000)) as t;"
-    cur.execute(sql)
+    sql = "SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(t.*)::json) ) FROM (select id, name, alt_name, ST_Transform( geom, 4326 ) as geom from merged_snap_polys m where ST_DWithin(m.geom, 'SRID=3338;POINT(%s %s)', %s)) as t;"
+    cur.execute(sql, params)
     res = cur.fetchone()  # because GeoJSON, just the one line is needed.
     cur.close()
     geoJson = json.dumps(res)  # Convert to stringy-thing.
